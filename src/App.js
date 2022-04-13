a8762bbb9fa3816c02e5afe13f8f51977584660a
Amruth Kiran <amruthkiran47@gmail.com>
2022-04-13 18:51:59 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './Search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=e8fb5518';
+const API_URL = 'https://www.omdbapi.com?apikey=e8fb5518';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
